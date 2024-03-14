7e5de324cdf8fd80c7510a13f3f4e6bf44bcec00
Krish004 <95554021+Krish004@users.noreply.github.com>
2024-03-14 14:10:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,6 +86,10 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
 
 SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
+EMOJIS = [
+        "👍", "❤‍🔥", "❤", "🔥", 
+        "🥰", "👻", "😁", "🤔"
+]
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
